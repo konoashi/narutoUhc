c03986f7ccad8dfffade6a4a37808da898ced5f6
DrasticLp <tarikdu137@gmail.com>
2021-03-06 22:01:52 +0100

    06/03/21-22:01

---
+++ src/main/java/com/narutouhc/plugin/Main.java
@@ -0,0 +1,44 @@
+package com.narutouhc.plugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin
+{
+    //Instance du plugin
+    private static Main instance;
+    
+    //Appelé quand le plugin se charge
+    @Override
+    public void onLoad()
+    {
+        instance = this;
+        Bukkit.getServer().getConsoleSender().sendMessage(getPrefix() + "§aChargement du plugin en cours");
+    }
+    
+    //Appelé quand le plugin est activé
+    @Override
+    public void onEnable()
+    {
+        Bukkit.getServer().getConsoleSender().sendMessage(getPrefix() + "§aPlugin activé avec succès");
+    }
+    
+    //Appelé quand le plugin est désactivé
+    @Override
+    public void onDisable()
+    {
+        Bukkit.getServer().getConsoleSender().sendMessage(getPrefix() + "§aPlugin désactivé avec succès");
+    }
+    
+    //Fonction publique pour récupérer l'instance du plugin
+    public static Main getInstance()
+    {
+        return instance;
+    }
+    
+    //Prefixe des messages du plugin
+    public String getPrefix()
+    {
+        return "§f[§6NarutoUhc§f] ";
+    }
+}
