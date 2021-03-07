91e52c709f0455ea64a2ed34678ad9994f81b13c
DrasticLp <tarikdu137@gmail.com>
2021-03-07 18:41:44 +0100

    07/03/21 - 18:41

--- src/main/java/com/narutouhc/plugin/utils/RolesUtils.java
+++ src/main/java/com/narutouhc/plugin/utils/RolesUtils.java
@@ -435,9 +435,14 @@ public class RolesUtils
             cmp = "§eun déserteur du vilage";
         }
 
-        p.sendMessage("§9========= Vous êtes " + cmp + " §9=========");
-        p.sendMessage(Strings.repeat(" ", (9+(cmp.length())+11)/2) + "§5Voici votre rôle : §d" + gp.getRole().name());
-        p.sendMessage("§9" + Strings.repeat("=", ("========= Vous êtes " + cmp + " =========").length()-8));
+        p.sendMessage("§9========================================\n");
+        String roleLine = "§5Voici votre rôle : §d" + gp.getRole().name();
+        String cmpLine = "§9Vous êtes " + cmp;
+        
+        p.sendMessage(Strings.repeat(" ", (("========================================".length() / 2) - cmpLine.length() - 4)) + cmpLine);
+        p.sendMessage(Strings.repeat(" ", (("========================================".length() / 2) - roleLine.length() - 4)) + roleLine + "\n");
+        
+        p.sendMessage("§9========================================");
 
         return role;
     }
