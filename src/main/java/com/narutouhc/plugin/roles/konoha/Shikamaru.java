1c3a9d71323f6524aed2f4d5524dea9d1a635a80
DrasticLp <tarikdu137@gmail.com>
2021-03-07 16:35:31 +0100

    07/03/21 - 16:35

--- src/main/java/com/narutouhc/plugin/roles/konoha/Shikamaru.java
+++ src/main/java/com/narutouhc/plugin/roles/konoha/Shikamaru.java
@@ -10,39 +10,69 @@ import com.narutouhc.plugin.roles.Role;
 
 public class Shikamaru extends Role
 {
+    private boolean available = true;
+
     public Shikamaru(Player player)
     {
         super(player);
         this.type = EnumRole.SHIKAMARU;
         Main.getInstance().konohas.add(player);
-        
+
         if(Main.getInstance().roles.containsKey(p))
         {
             Main.getInstance().roles.remove(p);
         }
-        
+
         Main.getInstance().roles.put(p, this);
     }
 
     public void freezePlayer(Player target)
     {
-        Location initial = target.getLocation().clone();
-        
-        new BukkitRunnable()
-        {   
-            int timer = 30;
-            
-            @Override
-            public void run()
+        if(this.available)
+        {
+            Location initial = target.getLocation().clone();
+
+            new BukkitRunnable()
             {
-                if(this.timer >= 1)
+                int timer = 30;
+
+                @Override
+                public void run()
                 {
-                    target.teleport(initial);
-                    this.timer --;
+                    if(this.timer >= 1)
+                    {
+                        target.teleport(initial);
+                        this.timer--;
+                    }
+                    else
+                        this.cancel();
                 }
-                else
-                    this.cancel();
-            }
-        }.runTaskTimer(Main.getInstance(), 0, 1l);
+            }.runTaskTimer(Main.getInstance(), 0, 1l);
+            
+            Player pl = this.p;
+            
+            pl.sendMessage(Main.getInstance().getPrefix() + "§cVous avez utilisé votre pouvoir, il sera rechargé dans §61 §cminute");
+            
+            new BukkitRunnable()
+            {
+                int timer = 60;
+
+                @Override
+                public void run()
+                {
+                    if(this.timer >= 1)
+                    {
+                        this.timer --;
+                    }
+                    else
+                    {
+                        pl.sendMessage(Main.getInstance().getPrefix() + "§aVotre pouvoir est §6prêt");
+                        this.cancel();
+                    }
+                }
+            }.runTaskTimer(Main.getInstance(), 0, 20l);
+            
+            this.available = true;
+        }
     }
 }
