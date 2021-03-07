d999b67f2e808ba5a67d759df87e33e008adbfbd
DrasticLp <tarikdu137@gmail.com>
2021-03-07 18:50:13 +0100

    207/03/21 - 18:50

--- src/main/java/com/narutouhc/plugin/utils/RolesUtils.java
+++ src/main/java/com/narutouhc/plugin/utils/RolesUtils.java
@@ -439,8 +439,8 @@ public class RolesUtils
         String roleLine = "§5Voici votre rôle : §d" + gp.getRole().name();
         String cmpLine = "§9Vous êtes " + cmp;
         
-        p.sendMessage(Strings.repeat(" ", (("========================================".length() / 2) + (cmpLine.length() / 2) + 4)) + cmpLine);
-        p.sendMessage(Strings.repeat(" ", (("========================================".length() / 2) + (roleLine.length() / 2) + 4)) + roleLine);
+        p.sendMessage(Strings.repeat(" ", (("========================================".length() / 2) - (cmpLine.length() / 2) + 4)) + cmpLine);
+        p.sendMessage(Strings.repeat(" ", (("========================================".length() / 2) - (roleLine.length() / 2) + 4)) + roleLine);
         
         p.sendMessage("§9========================================");
 
